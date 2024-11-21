571d4206571166383ee3bff4fa41590da9e2e88d
sahil <sahil27raj@gmail.com>
2024-11-21 22:03:35 +0530

    added the eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginReact.configs.flat.recommended,
+];
